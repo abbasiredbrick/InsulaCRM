42d04ec519c2182062aa40b25242802044570580
abbasiredbrick <abbasi.redbrick@gmail.com>
2026-09-12 20:04:18 +0400

    Rebrand to Keystone: platform logos, sidebar, email app-name

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -250,7 +250,7 @@ class SettingsController extends Controller
         try {
             $result = $provider->send(
                 $request->to,
-                __('This is a test message from :app.', ['app' => $tenant->name ?? 'InsulaCRM'])
+                __('This is a test message from :app.', ['app' => $tenant->name ?? config('app.name')])
             );
 
             if ($result) {
@@ -1128,7 +1128,7 @@ class SettingsController extends Controller
             \Illuminate\Support\Facades\Mail::raw(
                 __('This is a test email from :name. Your SMTP settings are working correctly!', ['name' => $tenant->name]),
                 function ($message) use ($user) {
-                    $message->to($user->email)->subject(__('Test Email from InsulaCRM'));
+                    $message->to($user->email)->subject(__('Test Email from :app', ['app' => config('app.name')]));
                 }
             );
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -97,12 +97,7 @@
                 </button>
                 <h1 class="navbar-brand navbar-brand-autodark" style="margin-bottom: 0;">
                     <a href="{{ route('dashboard') }}">
-                        @php $tenantLogo = auth()->user()->tenant->logo_path ?? null; @endphp
-                        @if($tenantLogo)
-                            <img src="{{ \Illuminate\Support\Facades\Storage::disk('public')->url($tenantLogo) }}" alt="{{ config('app.name') }}" style="max-height: 56px; max-width: 220px;">
-                        @else
-                            <img src="{{ asset('images/logo-white.png') }}" alt="{{ config('app.name') }}" style="max-height: 84px; max-width: 330px;">
-                        @endif
+                        <img src="{{ asset('images/logo-white.png') }}" alt="{{ config('app.name') }}" style="max-height: 84px; max-width: 330px;">
                     </a>
                 </h1>
                 <div class="collapse navbar-collapse" id="sidebar-menu">
